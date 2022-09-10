69e30ffb2e120501724c848225ae99affd4a5627
root <root@ip-172-31-86-76.ec2.internal>
2022-09-10 11:28:48 +0000

    added Dynamic variable script

---
+++ scripting/for.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Dynamic variable the value will be change the each execution
+
+echo "Enter your Name:"
+read name
+
+echo "My Best brother is $name, He is doing in MS in UK {United Kingdom}"

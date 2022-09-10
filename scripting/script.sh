ff958886873663c9f81934c299442f3ae5044e86
root <root@ip-172-31-86-76.ec2.internal>
2022-09-10 13:06:39 +0000

    modified the script

--- scripting/script.sh
+++ scripting/script.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 echo "Welcome to Devops!"
 
+echo "My File name is $0"
+echo "First parameter is $1"
+echo "second parameter is $2"
+echo "third parameter is $3"
+echo "fourth parameter is $4"
+echo "tenth parameter is ${10}"
+
+echo "What are the parameters used in the script $@"
+echo "Total Parametrers Count $#"

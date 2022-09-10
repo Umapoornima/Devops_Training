87169202734adaa923545d26ffe4f77ae1b52172
root <root@ip-172-31-86-76.ec2.internal>
2022-09-10 12:48:08 +0000

    created variable parameters script

---
+++ scripting/variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Variable parameter, The value can't be fixed at the run time we can pass the values
+
+echo "Enter your Friends Name:"
+read GYKGVLS
+echo "My friends $GYKGVLS Birday Dates"
+
+echo "Ganesh anna bourn in the month of June $1"
+echo "Yaswanth bourn in the month of January $2"
+echo "Gowtham bourn in the month of June $3"
+echo "Kiran bourn in the month of May $4"
+echo "Vignesh bourn in the month of September $5"
+echo "Lachi bourn in the month of September $6"
+echo "Swarna bourn in the month of December $7"

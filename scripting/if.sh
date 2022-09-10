1ffb186a2468fead1e91fbdaaf29292dd85757ad
root <root@ip-172-31-86-76.ec2.internal>
2022-09-10 11:14:33 +0000

    created static variable script

---
+++ scripting/if.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# static Variable through out the script value can't change
+
+sis="Rajitha Ponna"
+echo "I Love my Younger Sister $sis, She is Studying in Physiotherapy at SVIMS College in Tirupathi"

46482eb888d831c824527f3d408eca73cba46e34
root <root@ip-172-31-86-76.ec2.internal>
2022-09-10 11:54:54 +0000

    created environment variable script

---
+++ scripting/poori.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Environment Variable, the value neither be a Static or Dynamic , By using Export command we can execute the value
+
+Domain="AWS and Devops"
+
+echo "Enter your name:"
+read name
+
+echo "Hello $name, Welcome to $Domain Course in $Institute at Banglore!"
+

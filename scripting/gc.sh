288b680381b22113d70c9575bd20a945173afb0c
root <root@ip-172-31-86-76.ec2.internal>
2022-09-11 10:46:54 +0000

    modify the script

--- scripting/gc.sh
+++ scripting/gc.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+fun1(){
+for i in apple Orange Grapes Banana
+do
+	echo "Hello Fruit $i"
+done
+}
+
+fun2(){
 for myvar in poori sotta lachi
 do
 	for i in {1..3}
@@ -6,4 +15,6 @@ do
 		echo "Hello $myvar in $i"
 	done
 done
+}
 
+fun2

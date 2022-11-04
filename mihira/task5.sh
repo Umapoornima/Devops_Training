d0551a89a1bf86c5d3b41fd9add8ba25a4349e01
umapoornima <pumapoornima777@gamil.com>
2022-11-04 11:05:05 +0000

    added the task file

---
+++ mihira/task5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Write a shell script to wheather the package is installed or not!
+
+echo -e "enter your package name: \c"
+read package
+
+if rpm -qa | grep -q $package
+then
+	echo "$package is installed `rpm -qa $package`"
+else
+	echo "$package is not installed"
+fi
+	
